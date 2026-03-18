858be22bc2aedba17b767bab4ce2e552df6bba1e
Redrex07 <rohanguchhait2007@gmail.com>
2026-03-18 16:08:49 +0530

    UC1 Initialize train consist and display summary

--- src/TrainConsistApp.java
+++ src/TrainConsistApp.java
@@ -1,3 +1,15 @@
-public class TrainConsistApp
-{
+import java.util.*;
+
+public class TrainConsistApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=== Train Consist Management App ===");
+
+        List<String> trainConsist = new ArrayList<>();
+
+        System.out.println("Initial Bogie Count: " + trainConsist.size());
+
+        System.out.println("Train system initialized successfully.");
+    }
 }
